97ca64d9500efe676cd34a5e2e4db3b4306eb950
jbodoh7613 <john.joseph.bodoh@gmail.com>
2022-04-15 14:04:53 -0400

    Moved list to tuple conversion into its own function

--- John Bodoh Computer Vision Assignment 4.py
+++ John Bodoh Computer Vision Assignment 4.py
@@ -7,14 +7,13 @@ import cv2 as cv
 """
 Returns scale space for a given image as a tuple of tuples, where each tuple within the main tuple contains the images of a single octave.
 Each octave consists of multiple "scales" of the given image, where each scale is a Gaussian blur of the image which uses a sigma value k times the sigma value used for the previous scale
-For each successive octave, the image that is used is the image used in the previous octave, resized to half of its previous size
+For each successive octave, the image that is used is the image used in the previous octave, resized to half of its previous size.
 """
 # first starting sigma can be 1.6 or half of 2^(1/2)
 # starting sigma for each new octave should be middle sigma value used for previous octave
 def create_scale_space(img: np.ndarray, num_octaves: int = 4, num_scales: int = 5, sigma: float = 1.6, k: float = 1.414214):
     middle_scale = num_scales//2 # We use integer division as the floor of the true quotient will be the value of the looping variable when the middle scale is encountered
     octave_list = []
-    octave_list_tuples = []
     for i in range(num_octaves):
         octave_list.append([])
     img_scaled = img.copy()
@@ -27,9 +26,23 @@ def create_scale_space(img: np.ndarray, num_octaves: int = 4, num_scales: int = 
             current_sigma = k*current_sigma
         current_sigma = next_sigma
         img_scaled = cv.resize(img_scaled, (int(img_scaled.shape[0]/1.414), int(img_scaled.shape[1]/1.414)))
-    for i in range(len(octave_list)):
-        octave_list_tuples.append(tuple(octave_list[i]))
-    return(tuple(octave_list_tuples))
+    return(space_list_to_tuple(octave_list))
+
+"""
+This function takes a scale space tuple and returns a LoG (Laplacian of Gaussian) space represented as a tuple of tuples, where each tuple within the main tuple consists of the LoG images of its respective octave.
+LoG is calculated using Difference of Gaussians, where, for each image in each octave, a LoG image is produced by subtracting the image below from it.
+"""
+"""
+def create_log_space(scale_space: Tuple[Tuple[np.ndarray, ...], ...]):
+    for i in range(len(scale_space)):
+        for j in range(len(scale_space[i])):
+            for k in range(len(scale_space[i][j].shape[0]))
+                for l in range(len(scale_space[i][j].shape[1])):
+"""
+
+# Converts a list of lists of images, representing an image space, to a tuple of tuples of images
+def space_list_to_tuple(space_list: list[list[np.ndarray]]):
+    return(tuple(tuple(octave) for octave in space_list))
 
 def create_scale_space_test():
     blocks = cv.imread('blocks_L-150x150.png', cv.IMREAD_GRAYSCALE)
